f731307da9c59d2a8788bbc41f80b4266ce4a9df
justin-aquino <justin.aquino25@gmail.com>
2022-05-19 13:10:33 -0700

    padding adjust

--- src/components/pages/Portfolio/Project.jsx
+++ src/components/pages/Portfolio/Project.jsx
@@ -4,7 +4,7 @@ import { Card } from "react-bootstrap"
 const Project = ({project}) => {
   return (
     <>
-        <Card className=" mb-4 ml-4 card card-body" style={{backgroundColor: "gray"}}>
+        <Card className=" mb-5 card card-body" style={{backgroundColor: "gray"}}>
           <a href={project.deployedLink} target="_blank" className="project-card"> <Card.Img src={project.image}  /> </a>
           <Card.Body className="card-body">
               <Card.Title><h3>{project.name}</h3></Card.Title>
